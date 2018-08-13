8fc5bf8658a58749cd2014c6b484cc8f6f0a7b4c
Lucila Rizzi <lucilarizzi@gmail.com>
2018-08-13 15:18:35 -0300

    prueba 1

---
+++ Tarea1/HolaVS/HolaVS/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HolaVS
+{
+    class Program
+    {
+        public static void Saludar()
+        {
+            Console.Write("Holaaaaa \n \n");
+            Console.WriteLine("Holaaaaa linea ");
+            Console.WriteLine("Holaaaaa fucnion saludar");
+
+        }
+
+        static void Main(string[] args)
+        {
+            Saludar();
+            Console.Write("Holaaaaa \n \n"); 
+            
+        }
+
+      
+    }
+}
+
+
