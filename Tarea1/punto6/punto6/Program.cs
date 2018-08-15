585606178de87bdbbd09f3adb738c1e0f66ba1c3
Lucila Rizzi <lucilarizzi@gmail.com>
2018-08-15 17:40:00 -0300

    punto 6

---
+++ Tarea1/punto6/punto6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace punto6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            //int val = 0;
+            Console.Write("\nIngrese su numero\t ");
+            String numero = Console.ReadLine();
+            int digitos= numero.Length;
+            for (int i = 0; i < digitos; i++)
+            {
+              suma =suma +( (int)char.GetNumericValue(numero[i]));
+            }
+                
+               
+
+            Console.Write("\n----{0}----\n", suma);
+        }
+    }
+}
